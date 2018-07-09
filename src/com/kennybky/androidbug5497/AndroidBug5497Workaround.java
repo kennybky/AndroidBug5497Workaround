4fa6928ef5624e0a6a21c1c17f5a9caab3890acf
Adekola Togunloju <adekola@7generationgames.com>
2018-07-09 16:29:28 -0700

    Add bug workaround

---
+++ src/com/kennybky/androidbug5497/AndroidBug5497Workaround.java
@@ -0,0 +1,76 @@
+package com.kennybky.androidbug5497;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+
+
+public class AndroidBug5497Workaround extends CordovaPlugin {
+
+    private CordovaInterface cordova;
+    private CordovaWebView webView;
+
+    // For more information, see https://code.google.com/p/android/issues/detail?id=5497
+    // To use this class, simply invoke assistActivity() on an Activity that already has its content view set.
+
+    
+
+    private View mChildOfContent;
+    private int usableHeightPrevious;
+    private FrameLayout.LayoutParams frameLayoutParams;
+    private Activity activity;
+
+    @Override
+    public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
+
+        this.cordova = cordova;
+        this.webView = webView;
+        this.assistActivity(cordova.getActivity());
+    }
+
+    private void assistActivity(Activity activity) {
+        FrameLayout content = (FrameLayout) activity.findViewById(android.R.id.content);
+        mChildOfContent = content.getChildAt(0);
+        mChildOfContent.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            public void onGlobalLayout() {
+                possiblyResizeChildOfContent();
+            }
+        });
+
+        frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
+    }
+
+    private void possiblyResizeChildOfContent() {
+        int usableHeightNow = computeUsableHeight();
+        if (usableHeightNow != usableHeightPrevious) {
+            int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
+            int heightDifference = usableHeightSansKeyboard - usableHeightNow;
+            if (heightDifference > (usableHeightSansKeyboard/4)) {
+                // keyboard probably just became visible
+                frameLayoutParams.bottomMargin = heightDifference;
+            } else {
+                // keyboard probably just became hidden
+                frameLayoutParams.bottomMargin = 0;
+            }
+            mChildOfContent.requestLayout();
+            usableHeightPrevious = usableHeightNow;
+        }
+    }
+
+    private int computeUsableHeight() {
+        Rect r = new Rect();
+        mChildOfContent.getWindowVisibleDisplayFrame(r);
+        return (r.bottom - r.top);
+    }
+
+}
